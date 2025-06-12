29d712e8f5534e236eac5925c2ae8ae81775ced8
Tarek Ait Ahmed <159696248+tarek-ait@users.noreply.github.com>
2025-06-12 12:33:47 +0100

    updating the content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -87,6 +87,8 @@ export default async function About() {
                       href={`${profile?.resumeURL}?dl=${profile?.fullName}-resume.pdf`}
                       className="flex items-center justify-center text-center dark:text-primary-color text-secondary-color hover:underline basis-[10%] dark:bg-primary-bg bg-zinc-100 border border-transparent dark:hover:border-zinc-700 hover:border-zinc-200 rounded-md py-3 text-lg"
                       title="Download Resume"
+                      // disable
+
                     >
                       <BiSolidDownload
                         className="text-lg"
--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import PageHeading from "@/app/components/shared/PageHeading";
 
 const images = [
+  // update this with the images in the drive
   {
     id: "1",
     src: "https://images.unsplash.com/photo-1585618256754-241cfe4e8113?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
@@ -19,13 +20,13 @@ const images = [
 ];
 
 export const metadata: Metadata = {
-  title: "Photos | Victor Eke",
+  title: "Photos | Tarek Ait Ahmed",
   metadataBase: new URL("https://victoreke.com/photos"),
-  description: "Explore photos taken by Victor Eke",
+  description: "Explore photos taken by Tarek Ait Ahmed",
   openGraph: {
-    title: "Photos | Victor Eke",
+    title: "Photos | Tarek Ait Ahmed",
     url: "https://victoreke.com/photos",
-    description: "Explore photos taken by Victor Eke",
+    description: "Explore photos taken by Tarek Ait Ahmde",
     images:
       "https://res.cloudinary.com/victoreke/image/upload/v1692635149/victoreke/photos.png",
   },
@@ -36,7 +37,7 @@ export default function Photos() {
     <main className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20">
       <PageHeading
         title="Photos"
-        description="This page is still under construction..."
+        // description="This page is still under construction..."
       />
       <figure className="my-6">
         <Slide delay={0.12} className="flex flex-wrap gap-2">
@@ -44,7 +45,7 @@ export default function Photos() {
             <Image
               key={image.id}
               src={image.src}
-              alt="playing guitar"
+              alt=""
               width={350}
               height={800}
               className="dark:bg-primary-bg bg-secondary-bg"
